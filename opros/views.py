3833d21af1c684accada745ea8b6e7bbd386fd4b
Roman <roman161056@gmail.com>
2019-12-11 17:46:19 +0300

    Switch between questions

--- opros/views.py
+++ opros/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from opros.models import TextAnswer, Question
 
-def index(request):
-	return render(request, 'opros/opros.html')
+counter = 0
+
+
+def start_test(request):
+    global counter
+    counter = 0
+    return render(request, 'opros/startOpros.html')
+
+
+def get_question(request):
+    global counter
+    counter += 1
+    try:
+        q = Question.objects.get(id=counter)
+    except:
+        return render(request, 'opros/endOpros.html')
+    return render(request, 'opros/opros.html', {'question': q})
